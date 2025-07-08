f6a68dab250b30ef26ad852ef4e46787a946ccc6
Devendra-23 <dev6dhokare@gmail.com>
2025-07-08 10:54:24 +0530

    front end setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-3xl font-bold">
+        Job Tracker + AI Cover Letter Generator
+      </h1>
+    </div>
+  );
+}
+export default App;
